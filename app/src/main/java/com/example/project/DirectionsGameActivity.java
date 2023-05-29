3ecb1866b9fb8fdafb5f00642687fe141b2c8502
Taylan <mtaylanerol187@gmail.com>
2023-05-29 19:31:07 +0300

    play part ui and child activities ui finished

--- app/src/main/java/com/example/project/DirectionsGameActivity.java
+++ app/src/main/java/com/example/project/DirectionsGameActivity.java
@@ -2,10 +2,7 @@ package com.example.project;
 
 import android.os.Bundle;
 import android.view.View;
-import android.widget.Button;
-import android.widget.ImageView;
-import android.widget.TextView;
-import android.widget.Toast;
+import android.widget.*;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -15,13 +12,12 @@ public class DirectionsGameActivity extends AppCompatActivity implements View.On
 
     private ImageView imageView;
     private Button buttonNorth, buttonWest, buttonEast, buttonSouth;
-    private TextView answerTextView;
 
     private final int[] imageIds = {
-            R.drawable.image1,
-            R.drawable.image2,
-            R.drawable.image3,
-            R.drawable.image4
+            R.drawable.northdino,
+            R.drawable.eastdino,
+            R.drawable.westdino,
+            R.drawable.southdino
     };
 
     private final String[] correctAnswers = {
@@ -36,12 +32,37 @@ public class DirectionsGameActivity extends AppCompatActivity implements View.On
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_directions_game);
 
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+//Code for setting background
+        getWindow().getDecorView().setSystemUiVisibility(
+                View.SYSTEM_UI_FLAG_LAYOUT_STABLE
+                        | View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN
+                        | View.SYSTEM_UI_FLAG_FULLSCREEN);
+
+        if (getSupportActionBar() != null) {
+            getSupportActionBar().hide();
+        }
+
+// Create a FrameLayout to hold your SeamlessBackgroundView and the activity's layout
+        FrameLayout rootLayout = new FrameLayout(this);
+
+        // Create a SeamlessBackgroundView and add it to the FrameLayout
+        SeamlessBackgroundView backgroundView = new SeamlessBackgroundView(this, R.drawable.floating_tree, R.drawable.floating_tree_sky);
+        rootLayout.addView(backgroundView);
+
+        // Inflate the activity's layout and add it to the FrameLayout
+        View contentView = getLayoutInflater().inflate(R.layout.activity_directions_game, rootLayout, false);
+        rootLayout.addView(contentView);
+
+        // Set the FrameLayout as the content view
+        setContentView(rootLayout);
+///////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
         imageView = findViewById(R.id.imageView);
         buttonNorth = findViewById(R.id.buttonNorth);
         buttonWest = findViewById(R.id.buttonWest);
         buttonEast = findViewById(R.id.buttonEast);
         buttonSouth = findViewById(R.id.buttonSouth);
-        answerTextView = findViewById(R.id.answerTextView);
 
         buttonNorth.setOnClickListener(this);
         buttonWest.setOnClickListener(this);
@@ -56,7 +77,6 @@ public class DirectionsGameActivity extends AppCompatActivity implements View.On
         int randomIndex = random.nextInt(imageIds.length);
         imageView.setImageResource(imageIds[randomIndex]);
         imageView.setTag(imageIds[randomIndex]); // Set the tag with the image resource id
-        answerTextView.setText(""); // Clear the answer text
     }
 
     private void checkAnswer(String chosenAnswer) {
@@ -75,7 +95,6 @@ public class DirectionsGameActivity extends AppCompatActivity implements View.On
             return; // Exit the method without incrementing the index if the answer is incorrect.
         }
 
-        answerTextView.setText(correctAnswers[currentImageIndex]); // Display the correct answer
         showRandomImage();
     }
 
